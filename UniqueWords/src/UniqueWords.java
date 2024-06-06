8ab4e2abab68feba0641585b3d19718688a25a08
MaxDavydovQA <166731550+MaxDavydovQA@users.noreply.github.com>
2024-06-06 22:44:57 +0300

    Add files via upload

---
+++ UniqueWords/src/UniqueWords.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class UniqueWords {
+    public static void main(String[] args) {
+        String[] words = {
+                "apple", "banana", "apple", "orange", "banana", "grape",
+                "lemon", "grape", "apple", "melon", "cherry", "cherry",
+                "lemon", "apple", "grape", "orange", "banana", "kiwi"
+        };
+
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
+
+        System.out.println("Уникальные слова : " + uniqueWords);
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println("Количество вхождений каждого слова : " + wordCount);
+    }
+}
